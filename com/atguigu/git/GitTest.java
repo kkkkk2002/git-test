99a58e870157932866557346a33dee8151859a5e
zjl <2994505919@qq.com>
2022-07-23 10:39:41 +0800

    commit1

--- com/atguigu/git/GitTest.java
+++ com/atguigu/git/GitTest.java
@@ -8,6 +8,10 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("hello git2!");
         System.out.println("hello git2!");
+        System.out.println("hot-fix test");System.out.println("hot-fix test");
+
+
+
 
     }
 }
